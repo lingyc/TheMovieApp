ae52161028d73a5b8e9bdb74b58eb5381ef125b4
Hack Reactor Students <accounts@hackreactor.com>
2016-07-14 20:38:23 -0700

    Successfully authenticated and routed to home
    However, still need to handle CRUD and server get/post to database

--- public/components/App.js
+++ public/components/App.js
@@ -12,23 +12,32 @@ class App extends React.Component {
   changeView(){
 
     this.setState({
-      view:"SignUp"
+      view:"SignUp" 
     })
   }
 
 logInFunction(name,password){
-
+var that=this;
 console.log(name,password)
 
-  $.post('http://127.0.0.1:3000/login',{name:name,password:password}).then(function() {
-      console.log('success');
+  $.post('http://127.0.0.1:3000/login',{name:name,password:password}).then(function(response) {
+    console.log(response==='it worked')
+ 
+      if (response==='it worked'){
+     console.log('hi')
+        that.setState({
+          view:'Home'
+        })
+      }
+     console.log('this.state.view after state is set again',that.state.view)
     }).catch(function(){console.log('error')})
 }
 
   enterNewUser(name,password){
     console.log(name,password);
     $.post('http://127.0.0.1:3000/signup',{name:name,password:password}).then(function() {
-      console.log('success');
+      console.log('success'); 
+
     }).catch(function(){console.log('error')})
 
   }
@@ -40,11 +49,17 @@ console.log(name,password)
         ourFunction={this.changeView.bind(this)}
         logInFunction={this.logInFunction.bind(this)}
          / >  </div> );
-    } else {
+    } else if (this.state.view==="SignUp"){
       return ( < div >
         < SignUp enterUser={this.enterNewUser.bind(this)}/ >
         < /div>
       );
+    } else if (this.state.view==="Home"){
+ return ( < div >
+        Youre Home!!!
+        < /div>
+      );
+
     }
   }
 }
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var mysql = require('mysql');
 var handler = require('./lib/request-handler')
 var app = express();
 var bodyParser = require('body-parser');
+var sessions = require("client-sessions");
+
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -10,7 +12,14 @@ var con = mysql.createConnection({
   password: "12345"
 });
 
-
+app.use(sessions({
+  cookieName: 'mySession', // cookie name dictates the key name added to the request object
+  secret: 'blargadeeblargblarg', // should be a large unguessable string
+  resave: true,
+  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+  activeDuration: 1000 * 60 * 5, // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+  saveInitialized: true
+}));
 
 con.connect(function(err){
   if(err){
@@ -26,9 +35,14 @@ app.use(express.static(__dirname + '/public'));
 app.use('/scripts', express.static(__dirname + '/node_modules'));
 app.use('/compiled', express.static(__dirname + '/compiled'));
 
-app.post('/signup', handler.signupUser)
-app.post('/login', handler.signinUser)
+app.post('/signup', handler.signupUser);
+app.post('/login', handler.signinUser);
 
+app.get('/login',function(req,res){
+
+console.log('loeijwfloejfelifjdp')
+
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
