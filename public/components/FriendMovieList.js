09b91e39558960761a26114c62f223ef562e8ef0
Justin Ou <justinou@Justins-MacBook-Air.local>
2016-07-15 16:24:10 -0700

    Scaffolds for other views
    Mainly friendmovie and createmovie

---
+++ public/components/FriendMovieList.js
@@ -0,0 +1,21 @@
+let FriendMovieList = (props) => (
+  //have a nav bar and button
+  //depending on query on click
+  //map the recommended movies
+  <div className='friendMovieList'>
+
+    Movie Title: <input type ='text' 
+    id='movieInput' 
+    name='movieInput'
+    placeholder='Insert Movie Title'
+     />
+    <button onClick={function()
+      {props.getMovies(document.getElementById('movieInput'))}}>Get Movie</button>
+    <div>
+      <FriendMovieListEntry />
+    </div>
+
+  </div>
+);
+
+window.FriendMovieList = FriendMovieList;
