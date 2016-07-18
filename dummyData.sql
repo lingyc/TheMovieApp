98055f9ba5eb040cc7b9c46982d849b3f8a26a30
Ling Yu Chen <lingchen215@gmail.com>
2016-07-18 08:47:21 -0700

    Refactor getUserRating function to retrive all movies rated by user
    Also fix a mistake on dummyData where starwars is missing a poster field

--- dummyData.sql
+++ dummyData.sql
@@ -1,8 +1,8 @@
 -- mysql -u root -p
 
-INSERT INTO movies (id,title, genre, release_date, poster)
+INSERT INTO movies (id, title, genre, release_date, poster)
 VALUES (1,'matrix', 'scify', '1999', 'http://www.imagozone.com/var/albums/filme/The%20Matrix/The%20Matrix009.jpg?m=1292987658');
-INSERT INTO movies (id,title, genre, release_date)
+INSERT INTO movies (id, title, genre, release_date, poster)
 VALUES (2,'starwars', 'scify', '1989', 'http://i.kinja-img.com/gawker-media/image/upload/s---zKMfGT0--/c_scale,fl_progressive,q_80,w_800/19fk32sw3nt1wjpg.jpg');
 
 
@@ -40,3 +40,4 @@ INSERT INTO relations (user1id,user2id)
 VALUES (2, 3);
 INSERT INTO relations (user1id,user2id)
 VALUES (3, 2);
+
