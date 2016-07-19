9c8dad4dfe587346a524e6de5b22ff8dec530bd6
Ling Yu Chen <lingchen215@gmail.com>
2016-07-19 16:08:58 -0700

    Remove obsolete addMovie view from app.js and nav

--- public/components/Nav.js
+++ public/components/Nav.js
@@ -1,7 +1,6 @@
 let Nav = (props) => (
   <nav className="navbar">
     <div>
-        <button onClick={() => (props.onClick("recentRelease"))}>Home</button>
         <button id='logOutButton' onClick={props.logout}>Log Out</button> 
         <button onClick={() => (props.onClick("Home"))}>
         Home</button>
@@ -10,8 +9,6 @@ let Nav = (props) => (
         <button>Find New Movie Buddies(nyf)</button>
         <button onClick={() => (props.onClick("MovieSearchView"))}>
         IMDB Movie Ratings</button>
-        <button onClick={() => (props.onClick("Home2"))}>
-        Add Movie Ratings</button>
         <button onClick={() => (props.onClick("Friends"))}>Friends </button>
         <button onClick={() => (props.onClick("Inbox"))}>Inbox </button>
     </div>
