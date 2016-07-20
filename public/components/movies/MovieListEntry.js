904da40820f5356859ba9742058a0baeedb4bf31
Ling Yu Chen <lingchen215@gmail.com>
2016-07-20 15:48:21 -0700

    Attempt to add an external component but might need webpack

--- public/components/movies/MovieListEntry.js
+++ public/components/movies/MovieListEntry.js
@@ -4,25 +4,38 @@ class MovieListEntry extends React.Component {
     super(props);
 
     this.state = {
+    	userRating: null,
+    	friendAverageRating: null
     };
   }
 
+  componentWillMount() {
+  	this.setState({
+  		userRating: this.props.movie.score,
+  		friendAverageRating: this.props.movie.friendAverageRating
+  	})
+  }
+
+  onStarClick(name, value) {
+    this.setState({userRating: value});
+  }
+
+
   render() {
-  	var movie = this.props.movie;
-  	return(
-			<div className='movieEntry'>
-				<img className='moviethumnail' src={movie.poster}/>
-				<h1 className='movieTitle'>{movie.title}</h1>
-				<h1 className='movieYear'>{movie.year}</h1>
-				<p className='movieDescription'>{movie.description}</p>
-				<p className='userReview'>{(movie.review === '') ? movie.review : 'you have not review the movie yet'}</p>
-				<p className='imdbRating'>IMDB rating: {movie.imdbRating}</p>
-				<div className='watchRequestButton'>send watch request</div>
-				<div className='userRatingBlock'>user rating: {(movie.score) ? movie.score : 'you have not rate the movie'}</div>
-				<div className='avgFriendRatingBlock'>average friend rating: {(movie.friendAverageRating) ? movie.friendAverageRating : 'no friend ratings' }</div>
-			</div>
-		)
+  	let movie = this.props.movie
+  	return (
+  		<div className='movieEntry'>
+  			<img className='moviethumnail' src={movie.poster}/>
+  			<h1 className='movieTitle'>{movie.title}</h1>
+  			<p className='movieYear'>{movie.release_date}</p>
+  			<p className='movieDescription'>{movie.description}</p>
+  			<p className='userReview'>{(movie.review === '') ? movie.review : 'you have not review the movie yet'}</p>
+  			<p className='imdbRating'>IMDB rating: {movie.imdbRating}</p>
+  			<div className='watchRequestButton'>send watch request</div>
+  			<div className='userRatingBlock'>user rating: {(movie.score) ? movie.score : 'you have not rate the movie'}</div>
+  			<div className='avgFriendRatingBlock'>average friend rating: {(movie.friendAverageRating) ? movie.friendAverageRating : 'no friend ratings' }</div>
+  		</div>);
 	}
-)
+}
 
 window.MovieListEntry = MovieListEntry;
