370a487b8437d7018afa59bf8d281414afa33d90
Ling Yu Chen <lingchen215@gmail.com>
2016-07-20 11:47:14 -0700

    Change the watch and friend request table from integer to username

--- app/models/friendRequest.js
+++ app/models/friendRequest.js
@@ -4,12 +4,6 @@ var User = require('./user');
 //create friendRequest model
 var FriendRequest = db.Model.extend({
   tableName: 'friendRequests',
-  requestor: function() {
-    return this.belongsTo(User, 'requestor');
-  },
-  requestee: function() {
-    return this.belongsTo(User, 'requestee');
-  },
   hasTimestamps: true
 });
 
