20e25244c5a6eed196ee44e09447c54e76fc1a93
Hack Reactor Students <accounts@hackreactor.com>
2016-07-20 16:56:53 -0700

    Make some layout changes

--- FriendEntryView.js
+++ FriendEntryView.js
@@ -3,7 +3,7 @@ var FriendEntry = (props) => {
 
   return (
   <div className="FriendEntry">
-    <span id="Friend">Name:{props.Friend} </span>
+    <div id="Friend"><div><h3>{props.Friend}</h3></div>  <b>Compatability: TBD</b>  <button>Send watch request</button></div>
     <br/>
   </div>
 )};
