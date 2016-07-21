55712304700694fdab4f0e9c55ef9544fc486119
Justin Ou <justinou@Justins-Air.attlocal.net>
2016-07-20 23:37:25 -0700

    Accidently deleted branch
    and thus couldnt push to branch master.
    Checked out master instead, copy and pasted all files updated

---
+++ public/components/MovieFriendView.js
@@ -0,0 +1,11 @@
+let MovieFriendView = () => (
+  <div>
+    <h1>Star Trek</h1>
+    <div className="movieFriends">
+      <h5>Friend Ratings</h5>
+      <MovieFriendViewEntry />
+    </div>
+  </div>
+);
+
+window.MovieFriendView = MovieFriendView;
