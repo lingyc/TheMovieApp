734562b16d0e31a6aa1027ce3eaf353e6a2a6417
Justin Ou <justinou@Justins-Air.attlocal.net>
2016-07-23 10:56:20 -0700

    Removed signup drop
    not functional yet

--- public/components/SignUp.js
+++ public/components/SignUp.js
@@ -8,8 +8,6 @@ var SignUp = (props) => (
     id = 'SignUpPassword'
     name = 'SignUpPassword'/><br/> 
 
-    <SignUpDrop />
-
     <div id='buttons2'>
       <button onClick = {
         function() {
