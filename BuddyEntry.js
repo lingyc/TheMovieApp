ff1e2ad4d84a5f8ccfbff860e1654e67a95113b0
Krishan Arya <krishanmarya1@gmail.com>
2016-07-25 10:41:09 -0700

    Add %s

--- BuddyEntry.js
+++ BuddyEntry.js
@@ -2,7 +2,7 @@ var BuddyEntry = (props) => {
 //var test=props.Friend;
   return (
    <div>
-   <h3 >{props.Buddy}</h3><button className='buddy' onClick={function(){props.buddyfunc(props.Buddy)}}>Click to send {props.Buddy} a friend request</button> Compatability:{props.BuddyScore}
+   <h3 >{props.Buddy}</h3><button className='buddy' onClick={function(){props.buddyfunc(props.Buddy)}}>Click to send {props.Buddy} a friend request</button> Compatability:{props.BuddyScore}%
   </div>
 )
 };
--- FriendEntryView.js
+++ FriendEntryView.js
@@ -3,7 +3,7 @@ var FriendEntry = (props) => {
 
   return (
   <div className="FriendEntry">
-    <div id="Friend"><div><h3 className='individual' onClick={props.fof}>{props.Friend}</h3></div>  <b>Compatability: {props.Comp}</b>
+    <div id="Friend"><div><h3 className='individual' onClick={props.fof}>{props.Friend}</h3></div>  <b>Compatability: {props.Comp}%</b>
       <div className='watchModule'></div></div>
     <br/>
   </div>
