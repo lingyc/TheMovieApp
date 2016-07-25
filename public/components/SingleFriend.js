0ded3c79ebc063819aa0ac559c1298c071902e86
Ling Yu Chen <lingchen215@gmail.com>
2016-07-25 16:17:27 -0700

    More css on friend movie entry view

--- public/components/SingleFriend.js
+++ public/components/SingleFriend.js
@@ -1,28 +1,26 @@
 var SingleFriend = (props) => {
 
 
-console.log('props.moviesOfFriend',props.moviesOfFriend)
-if (props.moviesOfFriend.length === 0){
-	return (
-		<div>
-		<button onClick={() => (props.onClick("Friends"))}>Back to all friends</button><br/>
-		Sorry, {props.friendName.slice(props.friendName.indexOf(">")+1,props.friendName.lastIndexOf("<"))} hasn't rated any movies.
+	console.log('props.moviesOfFriend',props.moviesOfFriend)
+	if (props.moviesOfFriend.length === 0){
+		return (
+			<div>
+			<button onClick={() => (props.onClick("Friends"))}>Back to all friends</button><br/>
+			Sorry, {props.friendName.slice(props.friendName.indexOf(">")+1,props.friendName.lastIndexOf("<"))} hasn't rated any movies.
+			</div>
+			)
+
+	} else {
+		return (
+	  <div className="Home collection">
+			<a className="center waves-effect waves-light btn" onClick={() => (props.onClick("Friends"))}>Back to all friends</a>
+			<div className="header large"> list of {props.friendName}'s movies</div>
+			<div className='moviesOfFriend'>
+				{props.moviesOfFriend.map(movie => <MovieListEntry friendName={props.friendName} movie={movie} change={props.change}/> )}
+			</div>
 		</div>
 		)
-
-} else {
-	return (
-  <div>
-		<button onClick={() => (props.onClick("Friends"))}>Back to all friends</button>
-		<h2> List of {props.friendName}'s Movies</h2>
-		<div className='moviesOfFriend'>
-			{props.moviesOfFriend.map(movie => <MovieListEntry friendName={props.friendName} movie={movie} change={props.change}/> )}
-		</div>
-	</div>
-
-
-	)
-}
+	}
 };
 
 window.SingleFriend = SingleFriend;
