019c752c5344a8ebd9cd3aedeafc1f39628473a6
Ling Yu Chen <lingchen215@gmail.com>
2016-07-25 09:45:06 -0700

    Start styling myFriend's view

--- FriendEntryView.js
+++ FriendEntryView.js
@@ -2,10 +2,14 @@ var FriendEntry = (props) => {
 
 
   return (
-  <div className="FriendEntry">
-    <div id="Friend"><div><h3 className='individual' onClick={props.fof}>{props.Friend}</h3></div>  <b>Compatability: {props.Comp}</b>
-      <div className='watchModule'></div></div>
-    <br/>
+  <div className="FriendEntry collection-item row">
+  	<div className="col s3">
+  		<img className='profilethumnail' src={'https://unsplash.it/200/300/?random'}/>
+  	</div>
+    <div id="Friend" className="col s9">
+    	<h3 className='individual' onClick={props.fof}>{props.Friend}</h3>  
+    	<div>Compatability: {props.Comp}</div>
+    </div>
   </div>
 )};
 
--- Friends.js
+++ Friends.js
@@ -1,23 +1,14 @@
 var Friends = (props) => (
  
-  <div>
- <h1>Friends</h1>
- Enter friend you'd like to add here here:<input id='findFriendByName'></input> <button onClick={props.sendRequest}>Click to send request</button>
- <div style={{display:'none'}} id='enterRealFriend'>Please enter something!</div>
- <div style={{display:'none'}} id='reqSent'>Request sent!</div><br/>
- <div style={{display:'none'}} id='AlreadyReq'>Youve already sent a request to this user!</div><br/>
-
-
-<br/>
-<h2 >Here are your current friends</h2>
-
-
-{props.myFriends.map(function(friend){ return (<FriendEntry sendARequest={props.sendWatchRequest}  Friend={friend[0]} Comp={friend[1]} fof={props.fof} /> )})}
-
-
-
-
-
+  <div className="myFriends collection">
+		 <h1>Friends</h1>
+		 Enter friend you'd like to add here here:<input id='findFriendByName'></input> <button onClick={props.sendRequest}>Click to send request</button>
+		 <div style={{display:'none'}} id='enterRealFriend'>Please enter something!</div>
+		 <div style={{display:'none'}} id='reqSent'>Request sent!</div><br/>
+		 <div style={{display:'none'}} id='AlreadyReq'>Youve already sent a request to this user!</div><br/>
+		<br/>
+		<h2 >Here are your current friends</h2>
+		{props.myFriends.map(function(friend){ return (<FriendEntry sendARequest={props.sendWatchRequest}  Friend={friend[0]} Comp={friend[1]} fof={props.fof} /> )})}
   </div>
 
 
