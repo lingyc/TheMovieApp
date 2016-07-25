f1bb20c7086e00c47642edba854d616f53ea1275
Ling Yu Chen <lingchen215@gmail.com>
2016-07-25 13:38:13 -0700

    Continue styling work

--- public/components/BuddyEntry.js
+++ public/components/BuddyEntry.js
@@ -1,8 +1,15 @@
 var BuddyEntry = (props) => {
 //var test=props.Friend;
   return (
-   <div>
-   <h3 >{props.Buddy}</h3><button className='buddy' onClick={function(){props.buddyfunc(props.Buddy)}}>Click to send {props.Buddy} a friend request</button> Compatability:{props.BuddyScore}%
+  <div className='collection-item row'>
+    <div className="col s3">
+    	<img className='profilethumnail' src={'https://unsplash.it/170/170/?random'}/>
+    </div>
+    <div id="Friend" className="buddy col s9">
+   		<h3 className="buddyName">{props.Buddy}</h3>
+   		<a className="waves-effect waves-light btn" onClick={function(){props.buddyfunc(props.Buddy)}}>send friend request</a> 
+   		<div className="buddyCompatibility">Compatability:{props.BuddyScore}</div>
+  	</div>
   </div>
 )
 };
