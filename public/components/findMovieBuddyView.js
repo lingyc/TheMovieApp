478fe1bdd70f78b866acb2f59199bf7a813aebe3
Krishan Arya <krishanmarya1@gmail.com>
2016-07-25 12:09:23 -0700

    add another error message\

--- public/components/findMovieBuddyView.js
+++ public/components/findMovieBuddyView.js
@@ -1,10 +1,11 @@
 var FindMovieBuddy = (props) => {
-
+var empty=props.buddies.length===0?"You've friended everybody!":"";
   return (
 
    <div>
 
    <h2>Your Potential Movie Buddies</h2>  <br/>
+   {empty}
    <div style={{display:'none'}} id='AlreadyReq2'>Youve already sent a request to this user!</div><br/>
 
    {props.buddies.map(function(buddy){ if (buddy[1]===null){buddy[1]='Nothing to compare'} return (<BuddyEntry buddyfunc={props.buddyfunc} Buddy={buddy[0]} BuddyScore={buddy[1]} /> )})}
