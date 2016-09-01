8fea1c972a0f0a7c7b73758e859c738e22c42c00
The TARDIS <loaner@Mr-Kittys-MacBook-Air.local>
2016-09-01 15:20:40 -0700

    Add AirBNB linter

--- public/components/BuddyEntry.js
+++ public/components/BuddyEntry.js
@@ -6,7 +6,7 @@ const BuddyEntry = ({Buddy, BuddyScore}) => {
     </div>
     <div id="Friend" className="buddy col s9">
    		<h3 className="buddyName">{Buddy}</h3>
-   		<div className="buddyCompatibility">{(BuddyScore === 'Nothing to compare') ? 'Compatability: ' + Buddy + ' has not rated any movies' : 'Compatability: ' + props.BuddyScore }</div>
+   		<div className="buddyCompatibility">{(BuddyScore === 'Nothing to compare') ? `Compatability: ${Buddy} has not rated any movies` : `Compatability`${BuddyScore}`</div>
    		<a className="waves-effect waves-light btn" onClick={function(){buddyfunc(Buddy)}}>send friend request</a> 
   	</div>
   </div>
