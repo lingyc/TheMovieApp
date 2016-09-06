9904c80da4a18a773bf0f99c98f690c223a4edbb
Krishan Arya <krishanmarya1@gmail.com>
2016-09-06 19:41:09 +1000

    Begin to work on Windows

--- public/components/BuddyEntry.js
+++ public/components/BuddyEntry.js
@@ -1,16 +1,17 @@
-const BuddyEntry = ({Buddy, BuddyScore}) => {
-  return (
+const BuddyEntry = ({Buddy, BuddyScore,buddyfunc}) => (
+ 
   <div className='collection-item row'>
     <div className="col s3">
     	<img className='profilethumnail' src={'https://unsplash.it/170/170/?random'}/>
     </div>
     <div id="Friend" className="buddy col s9">
    		<h3 className="buddyName">{Buddy}</h3>
-   		<div className="buddyCompatibility">{(BuddyScore === 'Nothing to compare') ? `Compatability: ${Buddy} has not rated any movies` : `Compatability`${BuddyScore}`</div>
+      <div className="buddyCompatibility">{(BuddyScore === 'Nothing to compare') ? `Compatability: ${Buddy} has not rated any movies` : `Compatability${BuddyScore}`}</div>
    		<a className="waves-effect waves-light btn" onClick={function(){buddyfunc(Buddy)}}>send friend request</a> 
   	</div>
   </div>
 )
-};
+
 
 window.BuddyEntry = BuddyEntry;
+
